4473140a217cc3c82030e3b67f039e18d21f4910
Luis Manzo <manzosk3@Luiss-MacBook-Air.local>
2022-05-23 16:30:30 -0500

    initial commit, TODO: verify solution

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,26 @@
 function deafGrandma() {
-
+  alert('HEY KID');
+  let goodbyes = 0;
+  while(goodbyes < 2) {
+    let response = window.prompt();
+    if(response !== 'GOODBYE' && response.toUpperCase() === response) {
+      alert('NOT SINCE 1949');
+    } else if(response !== 'GOODBYE' && response.toUpperCase() !== response) {
+      alert('SPEAK UP SONNY');
+    }
+    if(response === 'GOODBYE') {
+      if(goodbyes === 0) {
+        alert('LEAVING SO SOON?');
+      }
+      if(goodbyes === 1) {
+        alert('SEE YA LATER ALLIGATOR');
+      }
+      goodbyes += 1;
+    }
+    if(goodbyes === 2) {
+      return;
+    }
+  }
 }
 
 deafGrandma();
